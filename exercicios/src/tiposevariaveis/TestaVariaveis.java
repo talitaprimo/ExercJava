3e003d4ac3113f0f16a09147720c6c3366d58c30
talitaprimo <talitareisprimo@gmail.com>
2018-12-21 17:56:19 -0200

    Tipos e variaveis

---
+++ exercicios/src/tiposevariaveis/TestaVariaveis.java
@@ -0,0 +1,8 @@
+package tiposevariaveis;
+
+public class TestaVariaveis {
+	public static void main(String[] args){
+		int idade = 37;
+		System.out.println("A idade é " + idade + ", parabéns!");
+	}
+}
