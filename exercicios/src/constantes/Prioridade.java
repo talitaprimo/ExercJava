b977ca483b9bedb9ff82acf4fbc009b57e5d1505
talitaprimo <talitareisprimo@gmail.com>
2019-11-27 15:14:56 -0200

    Aula Enum

---
+++ exercicios/src/constantes/Prioridade.java
@@ -0,0 +1,17 @@
+package constantes;
+
+public enum Prioridade {
+	MIN,
+	NORMAL,
+	MAX;
+}
+	
+//Usamos letras maiúsculas pois são valores constantes
+	
+/*	Exemplos clássicos de enumerações:
+	LIGADO, DESLIGADO;
+	PRIMAVERA, VERÃO, OUTONO, INVERNO;
+	NORTE, SUL, LESTE, OESTE; 
+*/
+	
+
