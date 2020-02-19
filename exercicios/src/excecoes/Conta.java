30482d645774b87dd83627673240bd4e5abb00f3
talitaprimo <talitareisprimo@gmail.com>
2020-02-19 16:58:53 -0300

    Aula 03 - Lançamento de exceções

---
+++ exercicios/src/excecoes/Conta.java
@@ -0,0 +1,7 @@
+package excecoes;
+
+public class Conta {
+
+	void deposita(){}
+	
+}
---
+++ exercicios/src/excecoes/Fluxo5.java
@@ -0,0 +1,32 @@
+package excecoes;
+
+//Aula 03 - Lançamento de exceções
+
+public class Fluxo5 {
+	public static void main(String[] args) {
+		System.out.println("Ini do main");
+		try {
+			metodo1();
+		} catch (ArithmeticException | NullPointerException ex) {
+			String msg = ex.getMessage();
+			System.out.println("Exception " + msg);
+			ex.printStackTrace();
+		}
+		System.out.println("Fim do main");
+	}
+
+	private static void metodo1() {
+		System.out.println("Ini do metodo1");
+		metodo2();
+		System.out.println("Fim do metodo1"); //Não é exibido pois saímos abruptamente do método
+	}
+
+	private static void metodo2() {
+		System.out.println("Ini do metodo2");
+		throw new ArithmeticException ("Ocorreu um erro!");  //Passamos para o construtor a mensagem "Ocorreu um erro!"
+		//System.out.println("Fim do metodo2");
+	}
+}
+		/*O Java reconhece que, quando jogamos uma exceção, saímos abruptamente do código. 
+		  Se isso acontece, não será possível executar a linha que mostra "Fim do metodo2". 
+		  Por isso, comentaremos essa linha do bloco.*/
